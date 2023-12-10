4b8851e2894e7ef8dacb681994d2b2d2c025d398
kormendidavidakos <davidakos45@gmail.com>
2023-12-10 19:12:11 +0100

    created prototype login request

--- src/server.js
+++ src/server.js
@@ -7,6 +7,9 @@ const PORT = 3001;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+const debugMode = true;
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -25,6 +28,32 @@ function validatePostRequest(fields, required, strict=false){
     if (!(search in fields)) return false;
   }
   return true
+}
+
+function throwDBError(res, err){
+  let msg = 'Internal Server Error';
+  if (debugMode) msg += ': ' + err;
+  res.status(500).send(msg);
+}
+
+function dbPostUserLogin(req, res){
+  data = req.body;
+  keys = Object.keys(data)
+  if (!validatePostRequest(keys, ["email", "password"])){
+    throwDBError(res, "Missing POST field(s)");
+  }
+  connection.query('SELECT * FROM user WHERE email = ? AND password = ?;', [data.email, data.password], (err, result) => {
+    if (err) {
+      throwDBError(res, err);
+    } else {
+      if (result.length > 0){
+        res.json({ success: true });
+      }
+      else{
+        res.json({ success: false })
+      }
+    }
+  });
 }
 
 connection.connect((err) => {
@@ -47,17 +76,12 @@ app.get('/api/data', (req, res) => {
   });
 });
 
-// app.post('/api/data', (req, res) => {
-//   const { name, value } = req.body;
-//   connection.query('INSERT INTO your_table (name, value) VALUES (?, ?)', [name, value], (err, result) => {
-//     if (err) {
-//       console.error('Error inserting into MySQL:', err);
-//       res.status(500).send('Internal Server Error');
-//     } else {
-//       res.json({ success: true });
-//     }
-//   });
-// });
+app.post('/api/data', (req, res) => {
+  
+});
+
+app.post('/api/login', (req, res) => dbPostUserLogin(req, res));
+
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
