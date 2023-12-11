edd8e29b1e0baa9a4ca524df877f4cf30654c242
kokasmark <mark.kokas04@gmail.com>
2023-12-11 19:32:09 +0100

    Created DietChart Component.

--- src/DietPage.js
+++ src/DietPage.js
@@ -6,6 +6,7 @@ import Sidebar from './Sidebar';
 import Navbar from './NavBar';
 import { PieChart } from 'react-minimal-pie-chart';
 import AuthRedirect from './authRedirect';
+import DietChart from './DietChart';
 
 class DietPage extends Component {
   state = {
@@ -30,59 +31,10 @@ class DietPage extends Component {
     return (
       <div>
         <div style={{ position: 'relative', left: '39vw', top: 250 }} className='chart'>
-        {(this.state.carbs > 0 || this.state.fat > 0 || this.state.proteins > 0) ? <div>
-            <h1 style={{ margin: 0, position: 'relative', left: 100, top: 220, color: 'white', textAlign: 'center', width: 200 }}>{this.state.calories} kcal</h1>
-            <div style={{position: 'absolute', top: -50, left: 400}}>
-              <ul style={{color:'white', listStyle: 'none'}}>
-                <li style={{marginBottom: -30}}>
-                  <div style={{width:30,height:30, borderRadius: 10, backgroundColor: '#1C1533'}}></div>
-                  <p style={{position:'relative', top:-28, left: 35}}>Carbs</p>
-                </li>
-                <li style={{marginBottom: -30}}>
-                  <div style={{width:30,height:30, borderRadius: 10, backgroundColor: '#3C6FAA'}}></div>
-                  <p style={{position:'relative', top:-28, left: 35}}>Fat</p>
-                </li>
-                <li>
-                  <div style={{width:30,height:30, borderRadius: 10, backgroundColor: '#10D8B8'}}></div>
-                  <p style={{position:'relative', top:-28, left: 35}}>Proteins</p>
-                </li>
-              </ul>
-            </div>
-            <div style={{ position: 'relative', left: -200, top: -200 }}>
-             <PieChart
-                label={(props) => { return props.dataEntry.title; }}
-                animate='true'
-                animationDuration={1000}
-                animationEasing="ease"
-                center={[100, 100]}
-                data={[
-                  {
-                    color: "#1C1533",
-                    title: "Carbs",
-                    value: this.state.carbs,
-                  },
-                  {
-                    color: "#3C6FAA",
-                    title: "Fat",
-                    value: this.state.fat,
-                  },
-                  {
-                    color: "#10D8B8",
-                    title: "Proteins",
-                    value: this.state.proteins,
-                  },
-                ]}
-                labelPosition={110}
-                lengthAngle={360}
-                lineWidth={50}
-                paddingAngle={0}
-                radius={50}
-                startAngle={0}
-                viewBoxSize={[200, 200]}
-                style={{ width: 800, height: 800 }}
-              />
-            </div>
-          </div> : <div style={{position: 'relative', top: 200}}><h1 style={{color: 'white'}}>There is no data for today.</h1></div>}
+          <div style={{position:'relative', left:-500, top: -250}}>
+            <DietChart/>
+          </div>
+        
           <div className='anim add-calorie'>
             <h1 style={{color: 'white', fontSize: 25, width:'100%', margin: 0}}>Add Calories Manually</h1>
             <ul style={{listStyle: 'none'}}>
