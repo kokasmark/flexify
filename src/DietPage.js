cdf5805b5f16790ec91cc5af077a0fffb2a6b1bd
kokasmark <mark.kokas04@gmail.com>
2023-12-15 17:11:46 +0100

    Diet Page Search Food and Add its nutritions.

--- src/DietPage.js
+++ src/DietPage.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
 import { ReactComponent as Icon_add } from './assets/icon-add.svg';
+import { ReactComponent as Icon_search } from './assets/icon-search.svg';
 import Sidebar from './Sidebar';
 import Navbar from './NavBar';
 import { PieChart } from 'react-minimal-pie-chart';
@@ -52,7 +53,79 @@ class DietPage extends Component {
 
    
   }
+  searchFood = () =>{
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("X-Api-Key", "CPyB7czRU+gk5oSYCEuxPA==QmXFanfZcubFgroY")
 
+    var requestOptions = {
+      method: 'GET',
+      headers: myHeaders,
+      mode: 'no-cors'
+    };
+
+    var query = document.getElementById('food-querry').value;
+
+    fetch('https://api.api-ninjas.com/v1/nutrition?query=' + query, {
+      method: 'GET',
+      headers: {
+        'X-Api-Key': 'CPyB7czRU+gk5oSYCEuxPA==QmXFanfZcubFgroY',
+      },
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok: ' + response.status);
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log(data[0]);
+        var d = data[0];
+        document.getElementById('food-calories').innerHTML = "Calories: "+ d.calories;
+        document.getElementById('food-fat').innerHTML = "Fat: "+ d.fat_total_g;
+        document.getElementById('food-carbs').innerHTML ="Carbs: "+ d.carbohydrates_total_g;
+        document.getElementById('food-protein').innerHTML ="Protein: "+ d.protein_g;
+      })
+      .catch(error => {
+        // Handle errors here
+        console.error('Fetch error:', error);
+      });
+    
+
+   
+  }
+  addFood(){
+    var p = Number(document.getElementById('food-protein').innerHTML.replace("Protein: ",''));
+    var c = Number(document.getElementById('food-carbs').innerHTML.replace("Carbs: ",''));
+    var f = Number(document.getElementById('food-fat').innerHTML.replace("Fat: ",''));
+    
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    var raw = JSON.stringify({token: localStorage.getItem('loginToken'), carbs: c
+    , fat: f, protein: p});
+
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
+    };
+
+    fetch("http://localhost:3001/api/diet/add", requestOptions)
+      .then(response => response.text())
+      .then((response) => {
+        console.log(response)
+        var r = JSON.parse(response);
+        if(r.success){
+          this.setState({carbs: this.state.carbs + c,fat: this.state.fat + f,proteins: this.state.proteins + p})
+
+          this.setState({calories: ((this.state.carbs+c) * 4)+((this.state.fat+f) * 9)+((this.state.proteins+p) * 4)})
+        }else{
+          
+        }
+      })
+      .catch(error => console.log('error', error));
+  }
   render() {
     return (
       <div className='page'>
@@ -78,6 +151,17 @@ class DietPage extends Component {
               </li>
             </ul>
             <Icon_add className='interactable' onClick={this.addCaloriesManually}/>
+          </div>
+          <div className='anim add-calorie load-anim' style={{position: 'absolute', top: 300}}>
+            <h1 style={{color: 'white', fontSize: 25, width:'100%', margin: 0}}>Add Food</h1>
+            <input id='food-querry' placeholder='Example: 10g Chicken'></input><Icon_search className='interactable' onClick={this.searchFood}/>
+            <ul style={{textAlign: 'start', color: 'white'}}>
+              <li><p id='food-calories'></p></li>
+              <li><p id='food-fat'></p></li>
+              <li><p id='food-protein'></p></li>
+              <li><p id='food-carbs'></p></li>
+            </ul>
+            <Icon_add className='interactable' onClick={this.addFood}/>
           </div>
         </div>
         <NavBarWrapper />
