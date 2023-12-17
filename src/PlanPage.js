83ce7a64bfc3939bdca2368985c90d6b66656ef4
kokasmark <mark.kokas04@gmail.com>
2023-12-17 19:41:16 +0100

    Visual big fixed releated to Diet Chart

--- src/PlanPage.js
+++ src/PlanPage.js
@@ -41,7 +41,7 @@ class PlanPage extends Component {
 
           </div>
           <h1 className='load-anim' style={{ position: 'relative', color: 'white', top: 300, left: 880 }}>{this.state.selectedDate}</h1>
-          <div style={{ position: 'absolute', top: 0, left: 300, zIndex: -1, transform: 'scale(0.5)' }}>
+          <div style={{ position: 'absolute', top: 120, left: 675, zIndex: -1, transform: 'scale(0.5)' }}>
             <DietChart hideInfo date={this.dateForapi} ref={this.dietRef} />
           </div>
         </div>
