d4db2bcd3c848eb21eb940e8b367e80ae8fb5709
kormendidavidakos <davidakos45@gmail.com>
2024-02-19 17:08:35 +0100

    the dumpsterfire, "testing" continues

--- server/test.js
+++ server/test.js
@@ -3,43 +3,71 @@ class Test{
         this.test = require('node:test')
         this.data = {}
 
-        this.runTests()
+        this.callAPIs()
         // this.sendRequest('GET', '/api/user', '9767afbb0ec883ba2e896b07f72237b0b2c9a0ade7a796b438789f64386bb5f1', this.testLogin)
     }
 
-    sendRequest(method, path, body, token,  callback, self){
-        console.log('---------------')
-        console.log(body)
-        console.log(method)
-        var request = require('request');
-        var options = {
-        'method': method,
-        'url': 'http://localhost:3001' + path,
-        'headers': {},
-        'body': JSON.stringify(body)
+    runTests(self){
+        console.log(self.data)
+    }
+
+    sendRequest(method, path, body, token,  callback){
+        const options = {
+            'method': method,
+            'url': 'http://localhost:3001' + path,
+            'headers': {'content-type' : 'application/json'},
+            'body': JSON.stringify(body),
+            
         };
-        if (token) options.headers['X-Token'] = token
+        if (token) options.headers['X-Token'] = this.data.token
         
+        console.log(`${path}:\t${this.data.token}`)
+
+        const self = this
+        const request = require('request')
         request(options, function (error, response) {
             if (error) throw new Error(error);
-            callback(response.body, self);
+            callback(JSON.parse(response.body), self);
         });
     }
 
-    runTests(){
-        // TODO: fix THIS shit
-        this.sendRequest('POST', '/api/login', {"username": "gipsz_jakab", "password": "teszt"}, undefined, this.testUserLogin, this)
-        this.sendRequest('GET', '/api/user', {}, this.data.token, this.testUserData, this)
+    async callAPIs(){
+        this.sendRequest('POST', '/api/signup', {username: "teszt123", email: "teszt@teszt.com", password: "teszt", location: "web"}, false, this.testRegister)
+        this.sendRequest('POST', '/api/login', {username: "gipsz_jakab", password: "teszt", location: "web"}, false, this.testLogin)
+
+        await this.isTokenSet() // wait for user token, all the calls below need it
+        this.sendRequest('GET', '/api/user', {}, true, this.testUser)
+
+        const self = this
+        const callback = this.runTests
+        setTimeout(function(){callback(self)}, 3000)
     }
 
-    testUserLogin(response, self){
+    testRegister(response, self){
+        self.data.register = response
+    }
+
+    testLogin(response, self){
         self.data.token = response.token
+        self.data.login = response
     }
 
-    testUserData(response, self){
-        if (!response.hasOwnProperty("username") || !response.hasOwnProperty("email")) return
-        self.data.userData = true
+    testUser(response, self){
+        self.data.user = response
     }
+
+    async isTokenSet(){
+        while (!this.data.login){
+            await this.delay(0.1)
+        }
+        return
+    }
+
+    delay(time) {
+        return new Promise(resolve => setTimeout(resolve, time));
+    }
+    
+
 
 
 }
